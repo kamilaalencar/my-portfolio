44f51611224cf0d411eba34a17413b5d9bd8a4cd
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-09-05 21:05:53 -0300

    fix:changing Navbar to portuguese

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -61,16 +61,16 @@ export const NavLinks = (): JSX.Element => {
   return (
     <NavbarLinks>
       <Button type="btLink" as="a" color="grey4" href={`#home`}>
-        Home
+        Quem Sou
       </Button>
       <Button type="btLink" as="a" color="grey4" href={`#projects`}>
-        Projects
+        Projetos
       </Button>
       <Button type="btLink" as="a" color="grey4" href={`#contact`}>
-        Contact
+        Contatos
       </Button>
       <Button type="btLink" as="a" color="grey4" href={`#social-media`}>
-        Social Media
+        Redes Socias
       </Button>
     </NavbarLinks>
   );
